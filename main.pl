8d4e6b7778e67d0999ed55eb274007fbbee7044f
andylolu2 <andylolu24@gmail.com>
2022-03-18 02:28:49 +0000

    Add comments

--- main.pl
+++ main.pl
@@ -1,6 +1,6 @@
 :- [gameTransitions].
 
-% evaluate(+State, -BestMove, -Value)
+% evaluate(+State, -BestMove, -Value) succeeds with the Value and BestMove at the give State.
 evaluate(State, _, 1) :- win(State, x).
 evaluate(State, _, -1) :- win(State, o).
 evaluate(state(Board, _), _, 0) :- allIn(Board, [o, x]). 
@@ -8,7 +8,9 @@ evaluate(state(Board, Player), BestMove, BestValue) :-
     next(state(Board, Player), Moves, NextStates), 
     bestStateValue(Player, Moves, NextStates, BestMove, BestValue).
 
-% bestStateValue(++Player, +Moves, +States, -BestMove, -BestValue)
+% bestStateValue(++Player, +Moves, +States, -BestMove, -BestValue) succeeds with the 
+% BestMove and BestValue of the most optimal move and state pair from Moves and States
+% for the current Player.
 bestStateValue(_, [Move], [State], Move, BestValue) :- evaluate(State, _, BestValue).
 bestStateValue(x, [Move|Moves], [State|States], BestMove, BestValue) :- 
     evaluate(State, _, Value1), 
